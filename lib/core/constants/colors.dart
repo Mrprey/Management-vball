dc6bb19b5e5250eeb08d8576485d27b01ba68e6d
Mrprey <prey.anderson2@gmail.com>
2025-06-15 12:13:10 -0300

    change app color

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -3,16 +3,17 @@ import 'package:flutter/material.dart';
 class AppColors {
   // Esta classe contém todas as cores do aplicativo
   // Light theme
-  static const Color primary = Colors.blue;
+  static const Color primary = Color(0xFFFFA726); // Laranja principal
+  static const Color primaryLight = Color(0xFFFFCC80); // Laranja claro
   static const Color secondary = Colors.green;
-  static const Color background = Colors.white;
+  static const Color background = Colors.white; // Fundo branco
   static const Color surface = Colors.white;
-  static const Color error = Colors.red;
-  static const Color text = Colors.black;
+  static const Color error = Colors.red; // Erro
+  static const Color text = Colors.black; // Texto preto
   static const Color border = Colors.grey;
 
   // Dark theme
-  static const Color primaryDark = Color(0xFF1565C0);
+  static const Color primaryDark = Color(0xFFF57C00); // Laranja escuro
   static const Color secondaryDark = Color(0xFF2E7D32);
   static const Color backgroundDark = Color(0xFF121212);
   static const Color surfaceDark = Color(0xFF1E1E1E);
